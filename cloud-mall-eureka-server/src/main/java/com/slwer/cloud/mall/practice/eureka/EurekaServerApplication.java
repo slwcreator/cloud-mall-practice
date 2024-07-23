5d896ba7ff57ec266d295f2c5c43641a51c86153
slwer <1047579002@qq.com>
2024-07-23 12:03:02 +0800

    feat: EurekaServer 开发

---
+++ cloud-mall-eureka-server/src/main/java/com/slwer/cloud/mall/practice/eureka/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.slwer.cloud.mall.practice.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
